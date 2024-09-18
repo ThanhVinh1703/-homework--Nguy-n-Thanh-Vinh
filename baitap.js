5892ebc41988252292ef91f4d0a32d2d580f0c5b
ThanhVinh1703 <nguyenthanhvinh170398@gmail.com>
2024-09-18 16:31:46 +0700

    bài tập về nhà

---
+++ baitap.js
@@ -0,0 +1,112 @@
+// bai tap 1
+
+// let year = +prompt("What year were you born?");
+// const present = 2024;
+// if(year < present){
+//     let age = present - year;
+//     console.log("your age is "+ age)
+// } else if( year > present){
+//     console.log("error!!!");
+// } else {
+//     console.log("Invalid input value.")
+// }
+
+
+
+// bai tap 2
+
+// let number = +prompt("enter an any numer your want");
+// let mod = number % 2;
+// if(mod == 0){
+//     console.log("the number is even");
+// }else if(mod > 0){
+//     console.log("the number is odd")
+// }else{
+//     console.log("Invalid input value.")
+// }
+
+
+// bai tap 3
+
+// let day = +prompt("enter a number of a week:");
+// switch(day){
+//     case day = 2 :{
+//         console.log("Thứ 2");
+//         break;
+//     }
+//     case day = 3 :{
+//         console.log("Thứ 3");
+//         break;
+//     }   
+//     case day = 4 :{
+//         console.log("Thứ 4");
+//         break;
+//     }  
+//     case day = 5 :{
+//         console.log("Thứ 5");
+//         break;
+//     }  
+//     case day = 6 :{
+//         console.log("Thứ 6");
+//         break;
+//     }  
+//     case day = 7 :{
+//         console.log("Thứ 7");
+//         break;
+//     }  
+//     case day = 8 :{
+//         console.log("Chủ nhật");
+//         break;
+//     }   
+//     default:{
+//         console.log("Invalid input value.")
+//     }
+// }
+
+
+
+// bai tap 4
+
+// let number = +prompt("Enter a number:");
+// number >= 0 ? console.log("so duong"):console.log("so am");
+
+
+// bai tap 5
+
+// let number = +prompt("Enter a number:");
+// for ( let i = 1 ; i <= number ; i++){
+//     console.log(i);
+// }
+
+
+// bai tap 6
+
+// let number = +prompt("Enter a number:");
+// let product = 1;
+// for( let i = 1 ; i <= number ; i++){
+//     console.log(i)
+//     product = product * i;
+//     console.log(number +"! = " + product);
+// }
+
+
+// bai tap 7
+// for(let i = 1 ; i <= 100 ; i++ ){
+//     console.log(i)
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log(i);
+//         console.log("FizzBuzz")
+//     }else if(i % 3 == 0){
+//         console.log(i);
+//         console.log("Fizz")
+//     }else if(i % 5 == 0){
+//         console.log(i);
+//         console.log("Buzz")
+//     }else{
+//         console.log("none")
+//     }
+// }
+
+
+
+
